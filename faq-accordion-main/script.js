c259ace964f4bd0f32fd3e69ae9580c32c77c807
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-02-15 22:42:30 -0800

    [FAQ Accordian] Removed active class and added comments

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -2,10 +2,12 @@ const accordianBtns = document.querySelectorAll(".accordian-btn");
 
 accordianBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    btn.classList.toggle("active");
+    // get the paragraph and icons
     const accordianDescription = btn.nextElementSibling;
     const plusIcon = btn.querySelector(".plus");
     const minusIcon = btn.querySelector(".minus");
+
+    // toggle the description - based on max height
     if (accordianDescription.style.maxHeight) {
       accordianDescription.style.maxHeight = null;
       plusIcon.style.display = "block";
